4677d4af7fa97f2a705ccf40e9d5948dab0842f4
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2023-06-27 11:43:07 -0300

    Create aula 13

---
+++ Modulo-02/aulas/aula13b.py
@@ -0,0 +1,4 @@
+print('Contagem de 1 até 6')
+for c in range(1, 7):
+    print(c)
+print('FIM')
---
+++ Modulo-02/aulas/aula13c.py
@@ -0,0 +1,4 @@
+print('Contagem regrassiva de 6 até 1')
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
