3dec281a8b49fe8d971b743de09123fc54149cb0
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2023-07-25 18:36:18 -0300

    up ex059

--- Modulo-02/exercicios/ex059.py
+++ Modulo-02/exercicios/ex059.py
@@ -11,10 +11,31 @@ n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
 opcao = 0
 while opcao != 5:
-    print('''[1] somar
+    print('''  [1] somar
     [2] multiplicar
     [3] maior
     [4] novos valores
     [5] sair do programa''')
-    opcao = str(input('Qual é a sua opção? '))
+    opcao = int(input('>>>> Qual é a sua opção? '))
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma entre {} e {} é {}'.format(n1, n2, soma))
+    elif opcao == 2:
+        produto = n1 * n2
+        print('O resultado de {} x {} é {}'.format(n1, n2, produto))
+    elif opcao == 3:
+        if n1 >= n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior é {}'.format(n1, n2, maior))
+    elif opcao == 4:
+        print('Informe os valores novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente.')
+    print('=-=' * 10)
 print('FIm do programa! Volte sempre!')
