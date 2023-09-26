14f5ffc128c565a6610ad21a8dfabfb7034aa725
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2023-09-25 21:13:27 -0300

    Create ex093

---
+++ Modulo-03/exercicios/ex093.py
@@ -0,0 +1,10 @@
+# Exercício 093
+# Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome
+# do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida.
+# No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+jogador = dict()
+partidas = list()
+jogador['nome'] = str(input('Nome do Jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, tot):
