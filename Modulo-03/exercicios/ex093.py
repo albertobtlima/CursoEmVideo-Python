bf869921e8b071004f956656f7d6786c1a4d2404
Alberto Lima <alberto.btlima@outlook.com>
2023-09-27 17:49:16 -0300

    Update ex093

--- Modulo-03/exercicios/ex093.py
+++ Modulo-03/exercicios/ex093.py
@@ -8,6 +8,16 @@ partidas = list()
 jogador['nome'] = str(input('Nome do Jogador: '))
 tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0, tot):
-    partidas.append(int(input(f'Quantos gols na partida {c}? ')))
+    partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-=' * 25)
 print(jogador)
-print(partidas)
+print('-=' * 20)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 25)
+print(f'O jogador {jogador["nome"]} jogou {tot} partidas.')
+for i, v in enumerate(jogador['gols']):
+    print(f'  => Na partida {i+1}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
