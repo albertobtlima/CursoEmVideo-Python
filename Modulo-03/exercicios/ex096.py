c7fa206c54f524292760ba0f7fb0ff134f9edb5a
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2023-09-30 15:12:25 -0300

    Create ex096

---
+++ Modulo-03/exercicios/ex096.py
@@ -0,0 +1,14 @@
+# Exercício 096
+# Faça um programa que tenha uma função chamada área(), que receba as dimensões
+# de um terreno terangular (largura e comprimento) e mostre a área do terreno.
+
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg} x {comp} é de {a}m²')
+
+
+print('Controle de Terrenos')
+print('-' * 20)
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l, c)
