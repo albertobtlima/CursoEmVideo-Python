69bdc306342306dbf35887bae8ba44083ec68da4
Alberto Lima <alberto.btlima@outlook.com>
2023-11-15 18:31:56 -0300

    Create ex110

---
+++ Modulo-03/exercicios/ex110/teste.py
@@ -0,0 +1,7 @@
+# Adicione ao módulo moeda.py criada nos desafios anteriores, uma função chamada resumo(),
+# que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado.
+
+import moeda
+
+preco = float(input('Digite o preço: R$'))
+moeda.resumo(preco)
